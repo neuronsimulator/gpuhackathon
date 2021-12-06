7d7f2343a8a6195265ee59cf7093b79eafbd18e3
Nicolas Cornu <nicolac76@yahoo.fr>
2021-12-06 09:58:52 +0100

    Replace openmp_example with Pramod's version

--- src/openmp_example.cpp
+++ src/openmp_example.cpp
@@ -1,69 +1,90 @@
-#include <cuda_profiler_api.h>
-
 #include <omp.h>
 #include <openacc.h>
 #include <iostream>
 
 #define N 10
 
-struct T {
+struct Data {
     int a;
     int *b;
 };
 
-void device_to_host(void* dst, void* src, size_t len) {
-    int r = omp_target_memcpy(dst, src, len, 0, 0, omp_get_initial_device(), omp_get_default_device());
-    if (r != 0) {
-        std::cout << "memcpy fail" << std::endl;
-    }
-}
-
-void host_to_device(void* dst, void* src, size_t len) {
-    int r = omp_target_memcpy(dst, src, len, 0, 0, omp_get_default_device(), omp_get_initial_device());
-    if (r != 0) {
-        std::cout << "memcpy fail" << std::endl;
-    }
-}
-
-int main() {
-    cudaProfilerStart();
-    T *t = new T;
+void acc_version() {
+    Data *t = new Data;
     t->a = 0;
     t->b = new int[N];
     for (unsigned i = 0; i < N; ++i) {
-        t->b[i] = 0x41;
-    }
-
-    // void* d_t = acc_copyin(t, sizeof(T));
-    // void* d_b = acc_copyin(t->b, sizeof(int) * N);
-    // acc_memcpy_to_device((void*)&(((T*)d_t)->b), &d_b, sizeof(int*));
-    auto host_id = omp_get_initial_device();
-    auto device_id = omp_get_default_device();
-    std::cout << "host_id: " << host_id << ", device_id: " << device_id << std::endl;
-    T* d_t = (T*)omp_target_alloc(sizeof(T), device_id);
-    host_to_device(d_t, t, sizeof(T));
-    int* d_b = (int*)omp_target_alloc(sizeof(int) * N, device_id);
-    host_to_device(d_b, t->b, sizeof(int) * N);
-    host_to_device(&(((T*)d_t)->b), &d_b, sizeof(int*));
-    omp_target_associate_ptr(t->b, d_b, sizeof(int) * N, 0, device_id);
-    omp_target_associate_ptr(t, d_t, sizeof(T), 0, device_id);
-
-    //#pragma acc parallel loop present(t[0:1])
-#pragma omp target loop map(to:t[:0])
-    for (unsigned i = 0; i < N; ++i) {
-#pragma omp atomic update
-        t->a++;
         t->b[i] = i;
     }
 
-    device_to_host(&(t->a), &(d_t->a), sizeof(int));
-    device_to_host(t->b, d_b, sizeof(int) * N);
+    void* d_t = acc_copyin(t, sizeof(Data));
+    void* d_b = acc_copyin(t->b, sizeof(int) * N);
+    acc_memcpy_to_device((void*)&(((Data*)d_t)->b), &d_b, sizeof(int*));
 
-    std::cout << t->a << std::endl;
+    #pragma acc parallel loop present(t[0:1])
+    for (unsigned i = 0; i < N; ++i) {
+        #pragma acc atomic update
+        t->a++;
+        t->b[i] *= 2;
+    }
+
+    acc_update_self(&(t->a), sizeof(int));
+    acc_update_self(t->b, sizeof(int) * N);
+
+    std::cout << "OpenACC : " <<  t->a << std::endl;
     for (unsigned i = 0; i < N; ++i) {
         std::cout << t->b[i] << std::endl;
     }
 
-    cudaProfilerStop();
+    acc_delete((void*)t->b, sizeof(int)*N);
+    acc_delete((void*)t, sizeof(Data));
+
+    delete [] t->b;
+    delete t;
+}
+
+void omp_version() {
+    Data *t = new Data;
+    t->a = 0;
+    t->b = new int[N];
+    for (unsigned i = 0; i < N; ++i) {
+        t->b[i] = i;
+    }
+
+    auto host_id = omp_get_initial_device();
+    auto device_id = omp_get_default_device();
+
+    Data* d_t = (Data*)omp_target_alloc(sizeof(Data), device_id);
+    omp_target_memcpy(d_t, t, sizeof(Data), 0, 0, omp_get_default_device(), omp_get_initial_device());
+
+    int* d_b = (int*)omp_target_alloc(sizeof(int) * N, device_id);
+    omp_target_memcpy(d_b, t->b, sizeof(int)*N, 0, 0, omp_get_default_device(), omp_get_initial_device());
+    omp_target_memcpy(&(d_t->b), &d_b, sizeof(int*), 0, 0, omp_get_default_device(), omp_get_initial_device());
+
+    omp_target_associate_ptr(t->b, d_b, sizeof(int) * N, 0, device_id);
+    omp_target_associate_ptr(t, d_t, sizeof(Data), 0, device_id);
+
+    #pragma omp target loop map(to:t[:0])
+    for (unsigned i = 0; i < N; ++i) {
+        #pragma omp atomic update
+        t->a++;
+        t->b[i] *= 2;
+    }
+
+    omp_target_memcpy(&(t->a), &(d_t->a), sizeof(int), 0, 0, omp_get_initial_device(), omp_get_default_device());
+    omp_target_memcpy(t->b, d_b, sizeof(int)*N, 0, 0, omp_get_initial_device(), omp_get_default_device());
+
+    std::cout << "OpenMP : " <<  t->a << std::endl;
+    for (unsigned i = 0; i < N; ++i) {
+        std::cout << t->b[i] << std::endl;
+    }
+
+    delete [] t->b;
+    delete t;
+}
+
+int main() {
+    acc_version();
+    omp_version();
     return 0;
 }
